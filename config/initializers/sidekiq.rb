1df1f25eb8838f09958b8a1aa923155e66a14af6
Taylor Lodge <taylor@boost.co.nz>
2016-01-20 09:39:03 +1300

    Configure Sidekiq to use Docker URL (needs to be changed)

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+Rails.logger.info("FOO")
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379/0' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis:6379/0' }
+end
